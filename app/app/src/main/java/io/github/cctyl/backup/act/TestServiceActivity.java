3802e61a4eb28d0ee1ee4ee6270561ffeedd84ae
cctyl <cccctyl@qq.com>
2025-07-27 20:24:20 +0800

    后台服务的基本架构

---
+++ app/app/src/main/java/io/github/cctyl/backup/act/TestServiceActivity.java
@@ -0,0 +1,96 @@
+package io.github.cctyl.backup.act;
+
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.function.Function;
+
+import io.github.cctyl.backup.R;
+import io.github.cctyl.backup.service.BackupService;
+
+public class TestServiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BackupService.LocalBinder binder;
+    
+    
+ 
+
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            binder = (BackupService.LocalBinder) service;
+
+            Log.d("TestServiceActivity", "onServiceConnected: 链接了");
+            binder.receiveCallback(s -> {
+
+
+                Log.d("TestServiceActivity", "receiveCallback: "+s);
+                return "";
+            });
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_service);
+        Button startService = (Button) findViewById(R.id.start_service);
+        Button stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+        Button bindService = (Button) findViewById(R.id.bind_service);
+        Button unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+        Button startIntentService = (Button) findViewById(R.id.start_intent_service);
+        startIntentService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, BackupService.class);
+                startService(startIntent); // 启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, BackupService.class);
+                stopService(stopIntent); // 停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, BackupService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE); // 绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection); // 解绑服务
+                break;
+            case R.id.start_intent_service:
+
+                break;
+            default:
+                break;
+        }
+    }
+}
